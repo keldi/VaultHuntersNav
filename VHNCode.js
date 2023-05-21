1408791754f86616e5fa94afbe3a30d8e7a482db
keldi <keldijunk@gmail.com>
2023-05-21 11:57:27 -0700

    K23 changes
    assorted experimentation from dead time at K23, plus some messing with grid structure and sizing on 05-21

--- VHNCode.js
+++ VHNCode.js
@@ -6,6 +6,11 @@ var oGridData;
 function funStartIt() {
 	gel("divOutput").innerHTML = ("startit");
 	oGridData = new GridData();
+	
+	oGridData.initialize(0, 0, 0, 0);
+	oGridData.movePlayer(oGridData.sThisPlayer, 0, 0);
+
+	/*
 	oGridData.initialize(-5, 5, -5, 5);
 	oGridData.addCell(0, 6);
 	oGridData.addCell(-2, -6);
@@ -15,8 +20,16 @@ function funStartIt() {
 	oGridData.addCell(1, -8);
 	oGridData.addCell(1, -7);
 	oGridData.addCell(1, -6);
+	*/
 }
 
+function funNodeSize(viMod) {
+	var oGrid = gel("divMapGrid");
+	var iCur = oGrid.style.getPropertyValue("--node-size");
+	iCur = iCur.slice(0, iCur.length-2);
+	iCur = parseInt(iCur) + parseInt(viMod);
+	oGrid.style.setProperty("--node-size", iCur + "px");
+}
 
 function funAddCell() {
 	gel("divOutput").innerHTML = ("funAddCell");
@@ -39,6 +52,9 @@ function GridData() {
 	this.c_asColClasses = ["mapGridCol"];
 	this.c_asNodeClasses = ["mapGridNode"];
 	this.bootstrap = false;
+	this.aoPlayers = [];
+	this.sThisPlayer = "YOU";
+	this.iSelectedLoc = [0, 0];
 
 	//Initialize grid data.
 	this.initialize = function (viRMin, viRMax, viCMin, viCMax) {
@@ -63,7 +79,45 @@ function GridData() {
 		//oFrame.appendChild(oCurC);
 		
 		this.setGrid(viRMin, viRMax, viCMin, viCMax);
+
+		gel("divActionN").onclick = this.passthroughActionClick.bind(this);
+		gel("divActionS").onclick = this.passthroughActionClick.bind(this);
+		gel("divActionE").onclick = this.passthroughActionClick.bind(this);
+		gel("divActionW").onclick = this.passthroughActionClick.bind(this);
+		gel("divActionC").onclick = this.passthroughActionClick.bind(this);
+		
+		this.sThisPlayer = "YOU";
+		this.addPlayer(this.sThisPlayer);
+		
+
 	};
+
+	this.addPlayer = function (vsPlayer) {
+		this.aoPlayers[String(vsPlayer)] = {name:String(vsPlayer), locX:0, locY:0, sColor:"playerRed", dUpdate:new Date()};
+	};
+
+	this.movePlayer = function (vsPlayer, vX, vY) {
+		var oPl = this.aoPlayers[String(vsPlayer)];
+
+		//Initialize room if doesn't exist
+		var sNextNN = this.gNN(parseInt(vX), parseInt(vY));
+		//in prog 2023-05-17: 12:47
+		if (!gel(sNextNN)) {
+			//create new node
+			var oFrame = gel(c_sGridFrame);
+			oCurC = this.makeCol(parseInt(vX), parseInt(vY));
+			oFrame.appendChild(oCurC);
+		}
+		//TODO:  Remove indicator from node
+		var sStartNN = this.gNN(oPl.locX, oPl.locY);
+		gel(sStartNN).classList.remove(oPl.sColor);
+		//TODO: Add indicator to Node
+		oPl.locX = parseInt(vX);
+		oPl.locY = parseInt(vY);
+		sStartNN = this.gNN(oPl.locX, oPl.locY);
+		gel(sStartNN).classList.add(oPl.sColor);
+		if (vsPlayer == this.sThisPlayer) {this.iSelectedLoc = [vX, vY];}
+	}
 
 	//Adjust current grid based on new size.
 	this.setGrid = function (viRMin, viRMax, viCMin, viCMax) {
@@ -163,22 +217,6 @@ function GridData() {
 		return oTmp;
 	};
 
-	this.makeNode = function (viR, viC) {
-		var oTmp = document.createElement("div");
-		oTmp.classList.add(String(this.c_sMapPrefix) + "Node");
-		oTmp.classList.add(...this.c_asNodeClasses);
-		oTmp.classList.add(...this.c_asMapClasses);
-		oTmp.style.border = "1px solid black";
-		oTmp.gridRowNum = parseInt(viR);
-		oTmp.gridColNum = parseInt(viC);
-		//oTmp.style.height = "50px";
-		//oTmp.style.width = "50px";
-		oTmp.id = this.gNN(viR, viC);
-		oTmp.onclick = this.passthroughClick.bind(this);//Bind lets me use object functions
-		oTmp.appendChild(document.createTextNode("" + viR + "," + viC));
-		return oTmp;
-	};
-
 	this.resetBase = function () {
 		var oTmp = document.createElement("div");
 		if (this.bootstrap == true) {oTmp.classList.add("container");}
@@ -187,8 +225,13 @@ function GridData() {
 		oTmp.classList.add("text-center");
 		
 		oTmp.id = "divMapGrid";
+		oTmp.style.setProperty("--node-size", "50px");
 		var oBase = gel("divMapBase");
+		var oPreserve = oBase.removeChild(gel("divNodeActions"));
+		
 		while (oBase.firstChild) { oBase.removeChild(oBase.firstChild); }
+		
+		oBase.appendChild(oPreserve);
 		oBase.appendChild(oTmp);
 		return oTmp;
 	};
@@ -196,11 +239,54 @@ function GridData() {
 	this.passthroughClick = function (event) {
 		//oGridData.printLog("passthroughClick[" + event + "]");
 		oGridData.nodeClick(event);
-	}
+	};
 
 	this.nodeClick = function (event) {
 		//this.printLog("nodeClick e.cT[" + event.currentTarget + "]  e.cT.id[" + event.currentTarget.id + "]");
 		this.printLog("nodeClick e.gRN[" + event.currentTarget.gridRowNum + "]  e.cT.gCN[" + event.currentTarget.gridColNum + "]");
+		//gel("divNAOutput").innerHTML = "e.gRN[" + event.currentTarget.gridRowNum + "]  e.cT.gCN[" + event.currentTarget.gridColNum + "]";
+		gel("divNodeActions").classList.remove("hidden");
+		this.iSelectedLoc[0] = parseInt(event.currentTarget.gridRowNum);
+		this.iSelectedLoc[1] = parseInt(event.currentTarget.gridColNum);
+	};
+
+	this.passthroughActionClick = function (event) {
+		oGridData.actionClick(event);
+	};
+
+	this.actionClick = function (event) {
+		//this.printLog("nodeClick e.gRN[" + event.currentTarget.gridRowNum + "]  e.cT.gCN[" + event.currentTarget.gridColNum + "]");
+		//gel("divNAOutput").innerHTML = "e.gRN[" + event.currentTarget.gridRowNum + "]  e.cT.gCN[" + event.currentTarget.gridColNum + "]";
+		//gel("divNodeActions").classList.remove("hidden");
+		if (event.currentTarget.id == "divActionC") {
+			this.movePlayer(this.sThisPlayer, this.iSelectedLoc[0], this.iSelectedLoc[1]);
+		}
+		if (event.currentTarget.id == "divActionN") {this.movePlayer(this.sThisPlayer, this.iSelectedLoc[0]-1, this.iSelectedLoc[1]);}
+		if (event.currentTarget.id == "divActionS") {this.movePlayer(this.sThisPlayer, this.iSelectedLoc[0]+1, this.iSelectedLoc[1]);}
+		if (event.currentTarget.id == "divActionW") {this.movePlayer(this.sThisPlayer, this.iSelectedLoc[0], this.iSelectedLoc[1]-1);}
+		if (event.currentTarget.id == "divActionE") {this.movePlayer(this.sThisPlayer, this.iSelectedLoc[0], this.iSelectedLoc[1]+1);}
+
+	};
+
+
+	
+
+	this.makeNode = function (viR, viC) {
+		var oTmp = document.createElement("div");
+		oTmp.classList.add(String(this.c_sMapPrefix) + "Node");
+		oTmp.classList.add(...this.c_asNodeClasses);
+		oTmp.classList.add(...this.c_asMapClasses);
+		oTmp.gridRowNum = parseInt(viR);
+		oTmp.gridColNum = parseInt(viC);
+		if (viR == 0 && viC == 0) {
+			oTmp.classList.add("nodeCenter");
+		}
+		//oTmp.style.height = "50px";
+		//oTmp.style.width = "50px";
+		oTmp.id = this.gNN(viR, viC);
+		oTmp.onclick = this.passthroughClick.bind(this);//Bind lets me use object functions
+		oTmp.appendChild(document.createTextNode("" + viR + "," + viC));
+		return oTmp;
 	};
 
 	this.printLog = function (vsLog) {
